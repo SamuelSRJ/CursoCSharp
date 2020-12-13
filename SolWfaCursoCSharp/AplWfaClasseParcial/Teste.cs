8e543362a9aea461cfb63dd77544c3d69cf8941d
Samuel de Souza <samuelsouzarosajr@gmail.com>
2020-12-13 20:16:52 -0300

    Commit da Aula #036 - Classes Parciais

---
+++ SolWfaCursoCSharp/AplWfaClasseParcial/Teste.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplWfaClasseParcial
+{
+    partial class Teste
+    {
+        // PROPRIEDADES DA CLASSE
+        public string Nome { get; set; }
+        public string Apelido { get; set; }
+    }
+}
---
+++ SolWfaCursoCSharp/AplWfaClasseParcial/Teste_metodos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplWfaClasseParcial
+{
+    partial class Teste
+    {
+        public void ApresentarNome()
+        {
+            Console.WriteLine(Nome);
+        }
+    }
+}
