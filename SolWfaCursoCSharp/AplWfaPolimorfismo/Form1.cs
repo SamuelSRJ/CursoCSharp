c77f950d0fa92404ca1284bc9affff9e149d2adf
Samuel de Souza <samuelsouzarosajr@gmail.com>
2020-12-20 20:54:32 -0300

    Commit da Aula #040 - Polimorfismo

---
+++ SolWfaCursoCSharp/AplWfaPolimorfismo/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplWfaPolimorfismo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            retangulo forma1 = new retangulo();
+            forma1.Largura = 50;
+            forma1.Altura = 100;
+            forma1.Apresentar();
+            forma1.Desenhar();
+            forma1.Desenhar_Outro();
+
+            triangulo forma2 = new triangulo();
+            forma2.Largura = 100;
+            forma2.Altura = 200;
+            forma2.Apresentar();
+            forma2.Desenhar();
+            forma2.Desenhar_Outro();
+
+            circunferencia forma3 = new circunferencia();
+            forma3.Largura = 250;
+            forma3.Altura = 300;
+            forma3.Apresentar();
+            forma3.Desenhar();
+            forma3.Desenhar_Outro();
+        }
+    }
+}
---
+++ SolWfaCursoCSharp/AplWfaPolimorfismo/classes.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplWfaPolimorfismo
+{
+    // ===================================================
+    abstract class forma_geometrica
+    {
+        int _largura;
+        int _altura;
+
+        // ----------------------------------------------------------
+        public int Largura
+        {
+            get { return _largura; }
+            set { _largura = value; }
+        }
+
+        // ----------------------------------------------------------
+        public int Altura
+        {
+            get { return _altura; }
+            set { _altura = value; }
+        }
+
+        // ----------------------------------------------------------
+        public void Apresentar()
+        {
+            Console.WriteLine("Este método pertence à classe base.");
+        }
+
+        // ----------------------------------------------------------
+        // DEVE SER IMPLEMENTADO POR TODAS AS CLASSE DERIVADAS
+        public abstract void Desenhar();
+
+        // ----------------------------------------------------------
+        public virtual void Desenhar_Outro()
+        {
+            Console.WriteLine("Método virtual da classe base.");
+        }
+    }
+
+    // ===================================================
+    class retangulo : forma_geometrica
+    {
+        public override void Desenhar()
+        {
+            Console.WriteLine("Desenha retangulo com as dimensões: " + Largura + "x" + Altura);
+        }
+
+        public override void Desenhar_Outro()
+        {
+            Console.WriteLine("Este é um método virtual substituido na classe derivada.");
+        }
+    }
+
+    // ===================================================
+    class triangulo : forma_geometrica
+    {
+        public override void Desenhar()
+        {
+            Console.WriteLine("Desenha triangulo com as dimensões: " + Largura + "x" + Altura);
+        }
+    }
+
+    // ===================================================
+    class circunferencia : forma_geometrica
+    {
+        public override void Desenhar()
+        {
+            Console.WriteLine("Desenha circunferencia com as dimensões: " + Largura + "x" + Altura);
+        }
+    }
+}
