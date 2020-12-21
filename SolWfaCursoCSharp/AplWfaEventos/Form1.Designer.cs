5056cb64e4c85f26b6aa031e8e285b9bafdbdb13
Samuel de Souza <samuelsouzarosajr@gmail.com>
2020-12-20 22:14:05 -0300

    Commit da Aula #043 - Eventos

---
+++ SolWfaCursoCSharp/AplWfaEventos/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace AplWfaEventos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBotao = new System.Windows.Forms.Button();
+            this.lblLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnBotao
+            // 
+            this.btnBotao.Location = new System.Drawing.Point(304, 290);
+            this.btnBotao.Name = "btnBotao";
+            this.btnBotao.Size = new System.Drawing.Size(190, 74);
+            this.btnBotao.TabIndex = 0;
+            this.btnBotao.Text = "Botão";
+            this.btnBotao.UseVisualStyleBackColor = true;
+            // 
+            // lblLabel
+            // 
+            this.lblLabel.BackColor = System.Drawing.Color.Transparent;
+            this.lblLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblLabel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblLabel.Location = new System.Drawing.Point(304, 137);
+            this.lblLabel.Name = "lblLabel";
+            this.lblLabel.Size = new System.Drawing.Size(190, 54);
+            this.lblLabel.TabIndex = 1;
+            this.lblLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblLabel.Click += new System.EventHandler(this.lblLabel_Click);
+            this.lblLabel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lblLabel_MouseDown);
+            this.lblLabel.MouseEnter += new System.EventHandler(this.lblLabel_MouseEnter);
+            this.lblLabel.MouseLeave += new System.EventHandler(this.lblLabel_MouseLeave);
+            this.lblLabel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.lblLabel_MouseUp);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(304, 236);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblLabel);
+            this.Controls.Add(this.btnBotao);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBotao;
+        private System.Windows.Forms.Label lblLabel;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ SolWfaCursoCSharp/AplWfaEventos/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplWfaEventos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblLabel_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Clicado");
+        }
+
+        private void lblLabel_MouseEnter(object sender, EventArgs e)
+        {
+            lblLabel.BackColor = Color.Yellow;
+        }
+
+        private void lblLabel_MouseLeave(object sender, EventArgs e)
+        {
+            lblLabel.BackColor = Color.Transparent;
+        }
+
+        private void lblLabel_MouseDown(object sender, MouseEventArgs e)
+        {
+            lblLabel.BackColor = Color.Red;
+        }
+
+        private void lblLabel_MouseUp(object sender, MouseEventArgs e)
+        {
+            lblLabel.BackColor = Color.Yellow;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            lblLabel.Text = textBox1.Text;
+        }
+    }
+}
