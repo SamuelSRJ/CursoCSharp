5325b719615e401080b17002e1c9fca04f7734fd
Samuel de Souza <samuelsouzarosajr@gmail.com>
2020-12-26 14:08:12 -0300

    Commit da Aula #050 - DateTime (Datas e Horas)

---
+++ SolWfaCursoCSharp/AplWfaDataHora/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace AplWfaDataHora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBotao = new System.Windows.Forms.Button();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDia = new System.Windows.Forms.TextBox();
+            this.txtMes = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAno = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBotao
+            // 
+            this.btnBotao.Location = new System.Drawing.Point(42, 200);
+            this.btnBotao.Name = "btnBotao";
+            this.btnBotao.Size = new System.Drawing.Size(176, 54);
+            this.btnBotao.TabIndex = 0;
+            this.btnBotao.Text = "Executar";
+            this.btnBotao.UseVisualStyleBackColor = true;
+            this.btnBotao.Click += new System.EventHandler(this.btnBotao_Click);
+            // 
+            // lblRes
+            // 
+            this.lblRes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.lblRes.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblRes.ForeColor = System.Drawing.Color.Red;
+            this.lblRes.Location = new System.Drawing.Point(337, 112);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(197, 48);
+            this.lblRes.TabIndex = 1;
+            this.lblRes.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Dia:";
+            // 
+            // txtDia
+            // 
+            this.txtDia.Location = new System.Drawing.Point(42, 45);
+            this.txtDia.Name = "txtDia";
+            this.txtDia.Size = new System.Drawing.Size(138, 22);
+            this.txtDia.TabIndex = 3;
+            // 
+            // txtMes
+            // 
+            this.txtMes.Location = new System.Drawing.Point(42, 99);
+            this.txtMes.Name = "txtMes";
+            this.txtMes.Size = new System.Drawing.Size(138, 22);
+            this.txtMes.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Mês:";
+            // 
+            // txtAno
+            // 
+            this.txtAno.Location = new System.Drawing.Point(42, 151);
+            this.txtAno.Name = "txtAno";
+            this.txtAno.Size = new System.Drawing.Size(138, 22);
+            this.txtAno.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ano:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 331);
+            this.Controls.Add(this.txtAno);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDia);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblRes);
+            this.Controls.Add(this.btnBotao);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBotao;
+        private System.Windows.Forms.Label lblRes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDia;
+        private System.Windows.Forms.TextBox txtMes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAno;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SolWfaCursoCSharp/AplWfaDataHora/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplWfaDataHora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBotao_Click(object sender, EventArgs e)
+        {
+            //lblRes.Text = DateTime.Now.ToString("yyyy/MM/dd");
+            //lblRes.Text = DateTime.Now.ToLongDateString();
+
+            //DateTime data = new DateTime(1998, 11, 28);
+            //lblRes.Text = data.DayOfWeek.ToString();
+
+            int dia = int.Parse(txtDia.Text);
+            int mes = int.Parse(txtMes.Text);
+            int ano = int.Parse(txtAno.Text);
+
+            DateTime data = new DateTime(ano, mes, dia);
+            lblRes.Text = data.DayOfWeek.ToString();
+        }
+    }
+}
