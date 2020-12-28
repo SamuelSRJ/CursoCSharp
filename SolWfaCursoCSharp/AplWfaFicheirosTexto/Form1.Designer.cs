3cd8f4826f2541a6d52f5d24559ac47b0874cf1e
Samuel de Souza <samuelsouzarosajr@gmail.com>
2020-12-28 19:12:01 -0300

    Commit da Aula #052 - Ficheiros de Texto

---
+++ SolWfaCursoCSharp/AplWfaFicheirosTexto/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace AplWfaFicheirosTexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.lstCaixa = new System.Windows.Forms.ListBox();
+            this.txtTexto = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Location = new System.Drawing.Point(312, 267);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(187, 54);
+            this.btnExecutar.TabIndex = 0;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // lstCaixa
+            // 
+            this.lstCaixa.FormattingEnabled = true;
+            this.lstCaixa.ItemHeight = 16;
+            this.lstCaixa.Location = new System.Drawing.Point(31, 33);
+            this.lstCaixa.Name = "lstCaixa";
+            this.lstCaixa.Size = new System.Drawing.Size(406, 164);
+            this.lstCaixa.TabIndex = 1;
+            // 
+            // txtTexto
+            // 
+            this.txtTexto.Location = new System.Drawing.Point(31, 212);
+            this.txtTexto.Name = "txtTexto";
+            this.txtTexto.Size = new System.Drawing.Size(406, 22);
+            this.txtTexto.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 379);
+            this.Controls.Add(this.txtTexto);
+            this.Controls.Add(this.lstCaixa);
+            this.Controls.Add(this.btnExecutar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.ListBox lstCaixa;
+        private System.Windows.Forms.TextBox txtTexto;
+    }
+}
+
---
+++ SolWfaCursoCSharp/AplWfaFicheirosTexto/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AplWfaFicheirosTexto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            // Gravar ficheiro de texto
+            // ASSINATURAS (LOCAL DO ARQUIVO E SEU NOME, APPEND, ENCONDING)
+            // APPEND TRUE -> Todo texto atualizado será ADIOCIONADO no arquivo
+            // APPEND FALSE -> Todo texto atualizado SUBSTITUIRÁ o conteudo no arquivo
+
+            int valor = 200;
+            StreamWriter ficheiro = new StreamWriter(@"C:\Users\usuario\Music\file.txt", true, Encoding.Default);
+            //ficheiro.WriteLine("Overwrited successfully!");
+            //ficheiro.WriteLine(valor);
+            ficheiro.WriteLine(txtTexto.Text);
+            txtTexto.Clear();
+            txtTexto.Focus();
+            ficheiro.Dispose();
+        }
+    }
+}
