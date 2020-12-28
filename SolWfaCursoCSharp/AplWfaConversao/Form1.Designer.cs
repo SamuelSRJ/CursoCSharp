1a6cf06063e7f0d60ad606ec303c3aa9d59178f8
Samuel de Souza <samuelsouzarosajr@gmail.com>
2020-12-28 20:54:42 -0300

    Commit da Aula #054 - Conversão de dados

---
+++ SolWfaCursoCSharp/AplWfaConversao/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace AplWfaConversao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.btnLer = new System.Windows.Forms.Button();
+            this.pcbCor = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbCor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.Location = new System.Drawing.Point(74, 291);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(191, 64);
+            this.btnGravar.TabIndex = 0;
+            this.btnGravar.Text = "Gravar";
+            this.btnGravar.UseVisualStyleBackColor = true;
+            this.btnGravar.Click += new System.EventHandler(this.btnGravar_Click);
+            // 
+            // btnLer
+            // 
+            this.btnLer.Location = new System.Drawing.Point(319, 291);
+            this.btnLer.Name = "btnLer";
+            this.btnLer.Size = new System.Drawing.Size(191, 64);
+            this.btnLer.TabIndex = 1;
+            this.btnLer.Text = "Ler";
+            this.btnLer.UseVisualStyleBackColor = true;
+            this.btnLer.Click += new System.EventHandler(this.btnLer_Click);
+            // 
+            // pcbCor
+            // 
+            this.pcbCor.Location = new System.Drawing.Point(367, 99);
+            this.pcbCor.Name = "pcbCor";
+            this.pcbCor.Size = new System.Drawing.Size(87, 78);
+            this.pcbCor.TabIndex = 2;
+            this.pcbCor.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 406);
+            this.Controls.Add(this.pcbCor);
+            this.Controls.Add(this.btnLer);
+            this.Controls.Add(this.btnGravar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pcbCor)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.Button btnLer;
+        private System.Windows.Forms.PictureBox pcbCor;
+    }
+}
+
