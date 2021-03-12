60cbf5f9c2861ca14b965ab3af4a1b0d1513cbc0
Samuel de Souza <samuelsouzarosajr@gmail.com>
2021-03-12 20:00:22 -0300

    Commit da Aula #57 - Pastas e Ficheiros parte 3

---
+++ SolWfaCursoCSharp/AplWfaApagarPastasFicheiros/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace AplWfaApagarPastasFicheiros
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.lstLista = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Location = new System.Drawing.Point(277, 245);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(125, 51);
+            this.btnExecutar.TabIndex = 0;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // lstLista
+            // 
+            this.lstLista.FormattingEnabled = true;
+            this.lstLista.Location = new System.Drawing.Point(12, 12);
+            this.lstLista.Name = "lstLista";
+            this.lstLista.Size = new System.Drawing.Size(390, 225);
+            this.lstLista.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 308);
+            this.Controls.Add(this.lstLista);
+            this.Controls.Add(this.btnExecutar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.ListBox lstLista;
+    }
+}
+
---
+++ SolWfaCursoCSharp/AplWfaApagarPastasFicheiros/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AplWfaApagarPastasFicheiros
+{
+    public partial class Form1 : Form
+    {
+        string pastaorigem = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\";
+        string pastadestino = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\temp\";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            string[] lista_ficheiros = Directory.GetFiles(pastaorigem, "*.txt");
+
+            foreach(string item in lista_ficheiros)
+            {
+                //lstLista.Items.Add(item);
+                //File.Move(item, Path.GetFileName(item));
+                File.Move(item, (pastadestino + Path.GetFileName(item)));
+            }
+
+            //File.Copy((pastaorigem + "arquivo1.txt"), (pastadestino + "arquivocopia.txt"));
+            //File.Move((pastaorigem + "arquivo1.txt"), (pastadestino + "arquivocopia.txt"));
+
+            //MessageBox.Show("Cópia realizada com sucesso!");
+        }
+    }
+}
