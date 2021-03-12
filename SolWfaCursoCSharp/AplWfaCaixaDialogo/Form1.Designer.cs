45471d3470d46a17a3ac501e5d15287a6f6dd9f5
Samuel de Souza <samuelsouzarosajr@gmail.com>
2021-03-12 20:56:05 -0300

    Commit da Aula #59 - Caixas de Dialogo

---
+++ SolWfaCursoCSharp/AplWfaCaixaDialogo/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace AplWfaCaixaDialogo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Location = new System.Drawing.Point(290, 263);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(126, 53);
+            this.btnExecutar.TabIndex = 0;
+            this.btnExecutar.Text = "Message Box";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 328);
+            this.Controls.Add(this.btnExecutar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecutar;
+    }
+}
+
---
+++ SolWfaCursoCSharp/AplWfaCaixaDialogo/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplWfaCaixaDialogo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            // MESSAGEBOX
+            // INFORMAÇÃO SIMPLES
+            // MessageBox.Show("MessageBox Simples!");
+
+            // TITULO BOTÕES E ICONES
+            // MessageBox.Show("Texto da Mensagem", "Titulo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            // CAIXA DE DIALOGO COM RESULTADO
+            if(MessageBox.Show("Deseja sair da aplicação?", "SAIR", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)return;
+
+            // FECHA A APLICAÇÃO
+            Application.Exit();
+
+        }
+    }
+}
