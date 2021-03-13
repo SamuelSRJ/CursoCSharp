fcc0c83ccc1150431e588737383602418026ff48
Samuel de Souza <samuelsouzarosajr@gmail.com>
2021-03-12 22:21:22 -0300

    Commit da Aula #61 - Caixas de Dialogo (SaveFile/FolderBrowser/ColorDialog)

--- SolWfaCursoCSharp/AplWfaCaixaDialogo/Form1.Designer.cs
+++ SolWfaCursoCSharp/AplWfaCaixaDialogo/Form1.Designer.cs
@@ -31,12 +31,15 @@
             this.btnExecutar = new System.Windows.Forms.Button();
             this.btnOpenFile = new System.Windows.Forms.Button();
             this.pcbImagem = new System.Windows.Forms.PictureBox();
+            this.btnSaveFile = new System.Windows.Forms.Button();
+            this.btnFolderBrowserDialog = new System.Windows.Forms.Button();
+            this.btnColorDialog = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pcbImagem)).BeginInit();
             this.SuspendLayout();
             // 
             // btnExecutar
             // 
-            this.btnExecutar.Location = new System.Drawing.Point(290, 204);
+            this.btnExecutar.Location = new System.Drawing.Point(290, 12);
             this.btnExecutar.Name = "btnExecutar";
             this.btnExecutar.Size = new System.Drawing.Size(126, 53);
             this.btnExecutar.TabIndex = 0;
@@ -46,7 +49,7 @@
             // 
             // btnOpenFile
             // 
-            this.btnOpenFile.Location = new System.Drawing.Point(290, 263);
+            this.btnOpenFile.Location = new System.Drawing.Point(290, 71);
             this.btnOpenFile.Name = "btnOpenFile";
             this.btnOpenFile.Size = new System.Drawing.Size(126, 53);
             this.btnOpenFile.TabIndex = 1;
@@ -58,16 +61,49 @@
             // 
             this.pcbImagem.Location = new System.Drawing.Point(12, 12);
             this.pcbImagem.Name = "pcbImagem";
-            this.pcbImagem.Size = new System.Drawing.Size(272, 304);
+            this.pcbImagem.Size = new System.Drawing.Size(272, 289);
             this.pcbImagem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pcbImagem.TabIndex = 2;
             this.pcbImagem.TabStop = false;
+            // 
+            // btnSaveFile
+            // 
+            this.btnSaveFile.Location = new System.Drawing.Point(290, 130);
+            this.btnSaveFile.Name = "btnSaveFile";
+            this.btnSaveFile.Size = new System.Drawing.Size(126, 53);
+            this.btnSaveFile.TabIndex = 3;
+            this.btnSaveFile.Text = "SaveFileDialog";
+            this.btnSaveFile.UseVisualStyleBackColor = true;
+            this.btnSaveFile.Click += new System.EventHandler(this.btnSaveFile_Click);
+            // 
+            // btnFolderBrowserDialog
+            // 
+            this.btnFolderBrowserDialog.Location = new System.Drawing.Point(290, 189);
+            this.btnFolderBrowserDialog.Name = "btnFolderBrowserDialog";
+            this.btnFolderBrowserDialog.Size = new System.Drawing.Size(126, 53);
+            this.btnFolderBrowserDialog.TabIndex = 4;
+            this.btnFolderBrowserDialog.Text = "FolderBrowserDialog";
+            this.btnFolderBrowserDialog.UseVisualStyleBackColor = true;
+            this.btnFolderBrowserDialog.Click += new System.EventHandler(this.btnFolderBrowserDialog_Click);
+            // 
+            // btnColorDialog
+            // 
+            this.btnColorDialog.Location = new System.Drawing.Point(290, 248);
+            this.btnColorDialog.Name = "btnColorDialog";
+            this.btnColorDialog.Size = new System.Drawing.Size(126, 53);
+            this.btnColorDialog.TabIndex = 5;
+            this.btnColorDialog.Text = "ColorDialog";
+            this.btnColorDialog.UseVisualStyleBackColor = true;
+            this.btnColorDialog.Click += new System.EventHandler(this.btnColorDialog_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(428, 328);
+            this.ClientSize = new System.Drawing.Size(428, 318);
+            this.Controls.Add(this.btnColorDialog);
+            this.Controls.Add(this.btnFolderBrowserDialog);
+            this.Controls.Add(this.btnSaveFile);
             this.Controls.Add(this.pcbImagem);
             this.Controls.Add(this.btnOpenFile);
             this.Controls.Add(this.btnExecutar);
@@ -84,6 +120,9 @@
         private System.Windows.Forms.Button btnExecutar;
         private System.Windows.Forms.Button btnOpenFile;
         private System.Windows.Forms.PictureBox pcbImagem;
+        private System.Windows.Forms.Button btnSaveFile;
+        private System.Windows.Forms.Button btnFolderBrowserDialog;
+        private System.Windows.Forms.Button btnColorDialog;
     }
 }
 
--- SolWfaCursoCSharp/AplWfaCaixaDialogo/Form1.cs
+++ SolWfaCursoCSharp/AplWfaCaixaDialogo/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace AplWfaCaixaDialogo
@@ -38,7 +39,7 @@ namespace AplWfaCaixaDialogo
         {
             OpenFileDialog caixa = new OpenFileDialog();
             caixa.Title = "Escolher ficheiro";
-            caixa.InitialDirectory = (Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+            caixa.InitialDirectory = (Environment.GetFolderPath(Environment.SpecialFolder.MyMusic));
             caixa.Filter = ".txt Files(*.txt)|*.txt|.doc Files(*.doc)|*.doc|.jpeg Files(*.jpg)|*.jpg";
 
             if(caixa.ShowDialog()== DialogResult.Cancel) return;
@@ -47,6 +48,42 @@ namespace AplWfaCaixaDialogo
             // MessageBox.Show("Ficheiro Selecionado: " + caixa.FileName);
             pcbImagem.BackgroundImage = Image.FromFile(caixa.FileName);
             caixa.Dispose();
+        }
+
+        private void btnSaveFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog caixa = new SaveFileDialog();
+            caixa.Title = "Gravar Ficheiro";
+            caixa.InitialDirectory = (Environment.GetFolderPath(Environment.SpecialFolder.MyMusic));
+            caixa.Filter = "TXT files(*.txt)|*.txt";
+            if (caixa.ShowDialog() == DialogResult.Cancel) return;
+
+            // GRAVA O FICHEIRO
+            string ficheiro = caixa.FileName;
+
+            StreamWriter file = new StreamWriter(ficheiro, false, Encoding.Default);
+            file.WriteLine("Teste");
+            file.Dispose();
+        }
+
+        private void btnFolderBrowserDialog_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog caixa = new FolderBrowserDialog();
+            caixa.SelectedPath = @"C:\";
+            if (caixa.ShowDialog() == DialogResult.Cancel) return;
+
+            // RETORNA A PASTA SELECIONADA
+            MessageBox.Show("A pasta escolhida foi: " + caixa.SelectedPath);
+        }
+
+        private void btnColorDialog_Click(object sender, EventArgs e)
+        {
+            ColorDialog caixa = new ColorDialog();
+            caixa.Color = pcbImagem.BackColor;
+            if (caixa.ShowDialog() == DialogResult.Cancel) return;
+
+            // ALTERA A COR DA PICTUREBOX
+            pcbImagem.BackColor = caixa.Color;
         }
     }
 }
