acaa1ab80c5ad7089a28ace1616cffa46d3ba66b
Samuel de Souza <samuelsouzarosajr@gmail.com>
2021-03-12 21:19:01 -0300

    Commit da Aula #60 - Caixas de Dialogo (OpenFileDialog)

--- SolWfaCursoCSharp/AplWfaCaixaDialogo/Form1.Designer.cs
+++ SolWfaCursoCSharp/AplWfaCaixaDialogo/Form1.Designer.cs
@@ -29,27 +29,52 @@
         private void InitializeComponent()
         {
             this.btnExecutar = new System.Windows.Forms.Button();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.pcbImagem = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbImagem)).BeginInit();
             this.SuspendLayout();
             // 
             // btnExecutar
             // 
-            this.btnExecutar.Location = new System.Drawing.Point(290, 263);
+            this.btnExecutar.Location = new System.Drawing.Point(290, 204);
             this.btnExecutar.Name = "btnExecutar";
             this.btnExecutar.Size = new System.Drawing.Size(126, 53);
             this.btnExecutar.TabIndex = 0;
-            this.btnExecutar.Text = "Message Box";
+            this.btnExecutar.Text = "MessageBox";
             this.btnExecutar.UseVisualStyleBackColor = true;
             this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(290, 263);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(126, 53);
+            this.btnOpenFile.TabIndex = 1;
+            this.btnOpenFile.Text = "OpenFileDialog";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // pcbImagem
+            // 
+            this.pcbImagem.Location = new System.Drawing.Point(12, 12);
+            this.pcbImagem.Name = "pcbImagem";
+            this.pcbImagem.Size = new System.Drawing.Size(272, 304);
+            this.pcbImagem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbImagem.TabIndex = 2;
+            this.pcbImagem.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(428, 328);
+            this.Controls.Add(this.pcbImagem);
+            this.Controls.Add(this.btnOpenFile);
             this.Controls.Add(this.btnExecutar);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pcbImagem)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -57,6 +82,8 @@
         #endregion
 
         private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.PictureBox pcbImagem;
     }
 }
 
--- SolWfaCursoCSharp/AplWfaCaixaDialogo/Form1.cs
+++ SolWfaCursoCSharp/AplWfaCaixaDialogo/Form1.cs
@@ -33,5 +33,20 @@ namespace AplWfaCaixaDialogo
             Application.Exit();
 
         }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog caixa = new OpenFileDialog();
+            caixa.Title = "Escolher ficheiro";
+            caixa.InitialDirectory = (Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+            caixa.Filter = ".txt Files(*.txt)|*.txt|.doc Files(*.doc)|*.doc|.jpeg Files(*.jpg)|*.jpg";
+
+            if(caixa.ShowDialog()== DialogResult.Cancel) return;
+
+            // ABRE O FICHEIRO
+            // MessageBox.Show("Ficheiro Selecionado: " + caixa.FileName);
+            pcbImagem.BackgroundImage = Image.FromFile(caixa.FileName);
+            caixa.Dispose();
+        }
     }
 }
