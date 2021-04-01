ab18b58d8e2f1407db2ba63934cdba87a5f69070
Samuel de Souza <samuelsouzarosajr@gmail.com>
2021-03-31 23:55:10 -0300

    Commit da Aula #68 - Propriedades de Font

---
+++ SolWfaCursoCSharp/AplWfaTipoDeLetra/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace AplWfaTipoDeLetra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLabel = new System.Windows.Forms.Label();
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblLabel
+            // 
+            this.lblLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLabel.Location = new System.Drawing.Point(12, 64);
+            this.lblLabel.Name = "lblLabel";
+            this.lblLabel.Size = new System.Drawing.Size(354, 69);
+            this.lblLabel.TabIndex = 0;
+            this.lblLabel.Text = "label1";
+            this.lblLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Location = new System.Drawing.Point(125, 185);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(130, 53);
+            this.btnExecutar.TabIndex = 1;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 312);
+            this.Controls.Add(this.btnExecutar);
+            this.Controls.Add(this.lblLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLabel;
+        private System.Windows.Forms.Button btnExecutar;
+    }
+}
+
---
+++ SolWfaCursoCSharp/AplWfaTipoDeLetra/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplWfaTipoDeLetra
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            Font letra = new Font("Helvetica", 18, FontStyle.Italic | FontStyle.Bold, GraphicsUnit.Pixel);
+            lblLabel.Font = letra;
+            lblLabel.ForeColor = Color.Yellow;
+            lblLabel.BackColor = Color.Black;
+        }
+    }
+}
